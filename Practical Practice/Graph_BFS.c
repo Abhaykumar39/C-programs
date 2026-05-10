51f8de507d50350a2a8ef330d3a5cb4d46bbff05
Abhakumar39 <Abhay.k7417@gmail.com>
2026-05-10 17:42:35 +0530

    code

--- Practical Practice/Graph_BFS.c
+++ Practical Practice/Graph_BFS.c
@@ -98,7 +98,7 @@ int main()
 {
     struct Graph *graph = createGraph(4);
 
-    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 1); 
     addEdge(graph, 0, 2);
     addEdge(graph, 1, 3);
     addEdge(graph, 2, 3);
--- Practical Practice/Selection_Sort.c
+++ Practical Practice/Selection_Sort.c
@@ -1,20 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Finding minimum element
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+
+        // Swapping
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
 
 int main()
 {
     int n = 5;
     int arr[n];
+
     printf("Enter the Elements: ");
+
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    insertionSort(arr, n);
+    selectionSort(arr, n);
 
     printf("After Sorting: ");
+
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
+
+    return 0;
 }
